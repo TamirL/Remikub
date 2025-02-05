eb942a905072d5f0af19625551b406afb586cfa3
TamirL <tamir168@gmail.com>
2025-02-05 15:17:04 +0200

    create joker images for tiles

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { getCardSizeContext, getCssCardColor, type CardData } from '$lib/domain/card';
 	import cardTileImage from '$lib/assets/card-tile.webp';
+	import jokerCardRedImage from '$lib/assets/joker-red.webp';
+	import jokerCardBlackImage from '$lib/assets/joker-black.webp';
 
 	let { cardData }: { cardData: CardData } = $props();
 	let cardColor = getCssCardColor(cardData);
@@ -13,8 +15,17 @@
 	style:background-image="url({cardTileImage})"
 	class="card"
 >
-	<div class="card-content" style:background-color={cardColor}>
-		{cardData.type === 'number' ? cardData.numericValue : 'JK'}
+	<div class="card-content">
+		{#if cardData.type === 'number'}
+			<div class="card-number" style:background-color={cardColor}>
+				{cardData.numericValue}
+			</div>
+		{:else}
+			<img
+				src={cardData.color === 'red' ? jokerCardRedImage : jokerCardBlackImage}
+				alt={`Joker ${cardData.color}`}
+			/>
+		{/if}
 	</div>
 </div>
 
@@ -40,13 +51,24 @@
 		.card-content {
 			font-size: 80cqmin;
 			font-weight: 500;
-			font-size: calc(var(--card-width) * 0.5);
-			margin-top: calc(var(--card-height) * 0.08);
-			color: transparent;
-			text-shadow: 2px 2px 3px rgba(255, 255, 255, 0.5);
-			-webkit-background-clip: text;
-			-moz-background-clip: text;
-			background-clip: text;
+
+			& img {
+				width: calc(var(--card-width) * 0.6);
+				height: calc(var(--card-width) * 0.6);
+				margin-top: calc(var(--card-height) * 0.15);
+			}
+
+			& .card-number {
+				color: transparent;
+				text-shadow: 2px 2px 3px rgba(255, 255, 255, 0.5);
+
+				font-size: calc(var(--card-width) * 0.5);
+				margin-top: calc(var(--card-height) * 0.08);
+
+				-webkit-background-clip: text;
+				-moz-background-clip: text;
+				background-clip: text;
+			}
 		}
 	}
 </style>
